0e96cdda6dc523b1e351d0e4b4a061ca6624f0c3
ittyi <vioiv.k4@gmail.com>
2022-08-24 20:31:40 +0900

    🩹 code clean

--- week2/itio/caeser/caesar.c
+++ week2/itio/caeser/caesar.c
@@ -7,26 +7,22 @@ string cesar_slide(string input_str, string inputStr);
 
 int main(int argc, string argv[])
 {
-	// printf("%d\n%s\n%s\n", argc, argv[0], argv[1]);
-	// 例外処理１
+	// errer check
 	if (argc != 2)
 	{
-		// printf("errer argc\n");
 		return 1;
 	}
 
-	// 例外処理１
+	// errer check
 	if (check_argv(argv[1]) == false)
 	{
-		// printf("errer argv\n");
 		return 1;
 	}
 
 	// input part
 	string input_str = get_string("plaintext: ");
-	// printf("input_str:%s\n", input_str);
 
-	// printf("main part\n");
+	// main part
 	string ans = cesar_slide(argv[1], input_str);
 	printf("ciphertext: %s\n", ans);
 }
@@ -35,7 +31,6 @@ bool check_argv(string num_str)
 {
 	for (int i = 0; num_str[i] != '\0'; i++)
 	{
-		// printf("numStr[i]:%c\n", num_str[i] );
 		if (num_str[i] < '0' || '9' < num_str[i])
 		{
 			return false;
@@ -46,8 +41,6 @@ bool check_argv(string num_str)
 
 bool check_range_azAZ(char input_char)
 {
-	// printf("check_range_azAZ:%c\n", input_char);
-	// printf("check_range_azAZ d:%d\n", input_char);
 	if (('a' <= input_char && input_char <= 'z') || ('A' <= input_char && input_char <= 'Z'))
 	{
 		return true;
@@ -55,42 +48,22 @@ bool check_range_azAZ(char input_char)
 	return false;
 }
 
-// barfoo
-// 23
-// ans: yxocll
-
 char convert_char(char plain_text, int argv)
 {
-	// printf("                      plain_text:%c argv:%d\n", plain_text, argv);
 	char ret_char = (char)(plain_text + argv);
-	// printf("(char)(plain_text + argv):%c\n", ret_char);
 	while (check_range_azAZ(ret_char) == false)
 	{
-		// printf("check\n");
-		if ('a' <= plain_text && plain_text <= 'z')
-		{
-			ret_char -= 26;
-			// printf("ret_char:%c\n", ret_char);
-			// printf("ret_char d:%d\n", ret_char);
-		} else if ('A' <= plain_text && plain_text <= 'Z')
-		{
-			ret_char -= 26;
-		}
-		// ret_char -= 24;
+		ret_char -= 26;
 	}
-	// printf("ret_char:%c\n", ret_char);
 	return ret_char;
 }
 
 string cesar_slide(string argv, string input_str)
 {
-	// printf("------------------argv:%s input_str:%s\n", argv, input_str);
 	for (int i = 0; input_str[i] != '\0'; i++)
 	{
-		// printf("for i[%d]:%c\n", i, input_str[i]);
 		if (check_range_azAZ(input_str[i]) == true)
 		{
-			// printf("true\n");
 			input_str[i] = convert_char(input_str[i], atoi(argv));
 		}
 	}
@@ -98,7 +71,7 @@ string cesar_slide(string argv, string input_str)
 }
 
 /*
-とりあえず、一文字ずるシーザーNumの分ずらしていこう。
+とりあえず、一文字ずつシーザーNumの分ずらしていこう。
 問題は、アルファベット以外にどう対処するか。
 if文で大文字か小文字の範囲内かを判定する関数を作る
 んで、範囲内だったらずらす関数に入れてcharをずらす
